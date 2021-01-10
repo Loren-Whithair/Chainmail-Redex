bcd941b02600cb4cd28354b51bb564dabd6b2127
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-01-11 11:39:52 +1300

    Featherweight Java racket language decl progress

--- featherweight-java-model.rkt
+++ featherweight-java-model.rkt
@@ -1,30 +1,59 @@
 #lang racket
 (require redex)
 
+; e: expression
+; L: class declaration
+;    (class [c_name] extends [c_super] {(list of field decls) // [Constructor] [Method decl...])
+
+; K: constructor declaration
+;    ([c_name] (([type] [field-name]) 
+;    
+; M: method declaration
+; C: class name
+; x: variable
+; f: field
+; 
+
+
+;semi-colons are replaced with // and extends is replaced with 'extends (it's a keyword)
+
 (define-language FJ
   (e ::=    
      x
      e.f
      e.m(e ...) ;method
-     (new C(e ...))
-     ((C)e))
-  (L ::= (class C 'extends C {(C f) ... // K M ...})) ;semi-colons are replaced with //   and extends is replaced with 'extends (it's a keyword)
-  (K ::= (C ((C f) ...) (super (f_1 ..._1) // (this. f_1 = f_1) ..._1)))
+     ('new C(e ...))
+     ((C) e))
+  (L ::= (class C 'extends C {(C f) ... K M ...})) 
+  (K ::= (C ((C f) ..._1) {super (f ...) // (this. f = f) ..._1}))
   (M ::= (C m (C x) ... (return e //)))
 
-  (f ::= variable-not-otherwise-mentioned) ; field Identifier
-  (x ::= variable-not-otherwise-mentioned) ; variable Identifier
-  (C ::= variable-not-otherwise-mentioned)) ;Class Identifier
+  (f ::= variable-not-otherwise-mentioned) ; fieldId
+  (x ::= variable-not-otherwise-mentioned
+     this) ; varId
+  (C ::= variable-not-otherwise-mentioned)) ; classId
+
+; Problems to solve:
+; - how to restrict the NAME of the constructor (i.e. we want the C in K to be the same as the class we are defining
+; - - judgment declaration, or reduction?
+
+; - restrict the number of constructor params to the number of fields in the class (this requires subbing K for it's decl in the decl of L)
+
 
 ; TODO
 ; define a program execution (define-extended-language prog FJ)
-
 ; give a judgment-form for subtypes
 
 
+;(define-language testing
+;  (te :=
+;      ((f ..._1) (ta. f = f) ..._1))
+;  (f ::= variable-not-otherwise-mentioned))
 
-(define-language testing
-  (te :=
-      ((f ..._1) (ta. f = f) ..._1))
-  (f ::= variable-not-otherwise-mentioned))
 
+
+
+(define e? (redex-match FJ e))
+(define L? (redex-match FJ L))
+(define K? (redex-match FJ K))
+(define M? (redex-match FJ M))
