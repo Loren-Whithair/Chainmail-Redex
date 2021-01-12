011cb35a60bff694313172bc69ac74312dea41be
David Franz <davidfranznz@gmail.com>
2021-01-12 16:07:45 +1300

    metafunctions

---
+++ Javalite/Javalite.rkt~
@@ -0,0 +1,77 @@
+#lang racket
+(require redex)
+
+
+(define-language Javalite
+  (P ::= (μ (C m)))
+  (μ ::= (CL ...))   ;class list
+  (T ::=  ;types
+     bool
+     unit
+     C)
+  (CL ::= (class C extends C ([T f] ...) (M ...)))  ;class decl
+  (M ::= (T m ([T x] ...) e))   ;method decl
+  (e ::=
+     x
+     v
+     (new C)
+     (e $ f)
+     (e @ m (e ...))  ;method invoc, with first e as the object the method is being invoked on and (e ...) the args
+     (e == e)
+     (C e)  ;typecast
+     (e instanceof C)
+     (x := e)
+     (x $ f := e)  ;field setting
+     (if e e else e)
+     (var T x := e in e)  ;var decl, where x is set to the first e, and the second e is the scope in which the var is used
+     (begin e ...))  ;a block of code
+  (x ::= this id)
+  (f ::= id)
+  (m ::= id)
+  (C ::= Object id)
+  (id ::= variable-not-otherwise-mentioned)
+  (pointer ::= (addr loc C) null)
+  (v ::=
+     pointer
+     true
+     false
+     unit
+     error)  ;variable
+  (loc ::= number))   ;numeric location in heap
+
+
+(define-extended-language
+  JL-Machine Javalite
+  
+  (e ::=  ;expression, (i.e. control string)
+     ....
+     (raw v @ m (v ...)))
+
+  (object ::= ((C [f loc] ...) ...)) ; storage format for Objects in the heap ↓↓↓
+  (hv ::= v object)
+  
+  (h ::=   ;heap
+     mt   ;empty
+     (h [loc -> hv]))  ;mapping of locations (numbers) to objects
+
+  (η ::=   ;local env
+     mt
+     (η [x -> loc]))  ;
+
+  (state ::= (μ h η e k))
+
+  (k ::=
+     ret
+     (* $ f -> k)
+     (* @ m (e ...) -> k)
+     (v @ m (v ...) * (e ...) -> k)
+     (* == e -> k)
+     (v == * -> k)
+     (C * -> k)
+     (* instanceof C -> k)
+     (x := * -> k)
+     (x $ f := * -> k)
+     (if * e else e -> k)
+     (var T x := * in e -> k)
+     (begin * (e ...) -> k)
+     (pop η k)))
---
+++ Javalite/metafunctions.rkt
@@ -0,0 +1,179 @@
+#lang racket
+(require redex)
+
+(define-metafunction javalite get-length : (any ...) -> number [(get-length (any_0 ...))
+,(length (term (any_0 ...)))])
+(define-metafunction javalite default-value : T -> v [(default-value bool)
+false] [(default-value unit)
+unit] [(default-value C)
+null ])
+(define-metafunction javalite default-value* : (T ...) -> (v ...) [(default-value* ())
+()]
+[(default-value* (T_0 T_1 ...))
+((default-value T_0) (default-value T_1) ...)])
+(define-metafunction javalite h-max : h -> number
+[(h-max mt) -1]
+[(h-max (h [loc -> hv]))
+,(max (term loc) (term (h-max h)))])
+(define-metafunction javalite h-malloc : h -> number [(h-malloc h)
+,(add1 (term (h-max h)))])
+ 59
+(define-metafunction javalite
+h-malloc-n-helper : number number -> (loc ...) [(h-malloc-n-helper number_b number_c)
+,(let ([z (term number_b)]) (build-list (term number_c) (lambda (y) (+ y z))))])
+(define-metafunction javalite h-malloc-n : h number -> (loc ...) [(h-malloc-n h number)
+(loc_0 ...)
+(where ((loc_0 ...)) (h-malloc-n* h number))])
+(define-metafunction javalite
+internal-h-malloc-n* : number (number ...) -> (number (loc ...) ...) [(internal-h-malloc-n* number_b (number_0))
+(number_t (loc_1 ...))
+(where (loc_1 ...) (h-malloc-n-helper number_b number_0)) (where number_t ,(if (empty? (term (loc_1 ...)))
+(term number_b)
+(add1 (apply max (term (loc_1 ...))))))] [(internal-h-malloc-n* number_b (number_0 number_1 number_2 ...))
+(number_t (loc_0 ...) (loc_1 ...) ...)
+(where (loc_0 ...) (h-malloc-n-helper number_b number_0)) (where number_i ,(if (empty? (term (loc_0 ...)))
+(term number_b)
+(add1 (apply max (term (loc_0 ...))))))
+(where (number_t (loc_1 ...) (internal-h-malloc-n*
+(define-metafunction javalite h-malloc-n* : h number ... -> [(h-malloc-n* h number_0 ...)
+...)
+number_i (number_1 number_2 ...)))])
+((loc ...) ...)
+(( loc_0 ...) ...)
+(where (number (loc_0 ...) ...) (internal-h-malloc-n* (h-malloc h) (number_0 ...)))])
+(define-metafunction javalite storelike-lookup : any any -> any [(storelike-lookup mt any_0)
+,(error ’storelike-loopup "~e␣not␣found␣in␣~e" (term any_0) (term mt))] [(storelike-lookup (any_0 [any_t -> any_ans]) any_t)
+any_ans]
+[(storelike-lookup (any_0 [any_k -> any_v]) any_t)
+60
+
+(storelike-lookup any_0 any_t)
+(side-condition (not (equal? (term any_k) (term any_t))))])
+(define (id-<= a b)
+(string <=? (symbol- >string a) (symbol- >string b)))
+(define (storelike-extend <= storelike k hv) (match storelike
+[’mt ‘(mt [,k -> ,hv])] [‘(,storelike [,ki -> ,hvi])
+(cond
+[(equal? k ki)
+‘(,storelike [,ki -> ,hv])] [(<= k ki)
+‘(,(storelike-extend <= storelike k hv) [,ki -> ,hvi])] [else
+‘((,storelike [,ki -> ,hvi]) [,k -> ,hv])])]))
+(define (storelike-extend* <= storelike extend*) (match extend*
+[’() storelike]
+[‘([,k -> ,hv] . ,extend*)
+(storelike-extend* <= (storelike-extend <= storelike k hv) extend*)]))
+(define-metafunction javalite h-lookup : h loc -> hv [(h-lookup h loc)
+(storelike-lookup h loc)])
+(define-metafunction javalite h-extend* : h [loc -> hv] ... -> h [(h-extend* h [loc -> hv] ...)
+,(storelike-extend* <= (term h) (term ([loc -> hv] ...)))])
+(define-metafunction javalite ?-lookup : ? x -> loc
+[(?- lookup ? x)
+(storelike-lookup ? x)])
+(define-metafunction javalite ?-extend* : ? [x -> loc] ... -> ? [(?-extend* ? [x -> loc] ...)
+61
+
+,(storelike-extend* id-<= (term ?) (term ([x -> loc] ...)))])
+(define-metafunction javalite restricted-field-lookup : object f -> loc [(restricted-field-lookup (C_c
+loc_target
+(define-metafunction javalite restrict-object : object ->
+object
+[f_0 loc_0] ...) ... [f_c loc_c] ...)
+[f_1 loc_1] ...) ...)) ...
+[(restrict-object (C_c
+(C_0 (C_c (C_1 ...)
+(C_0 [f_0 loc_0] ...) ... (C_t [f_t0 loc_t0] ...
+[f_target loc_target]
+[f_t1 loc_t1] ...) (C_1 [f_1 loc_1] ...) ...)
+f_target)
+;; Allows for redefinition and restricts matching
+;; to be the most recent definition by current cast. (side-condition
+(not (member (term f_target)
+(term (f_t1 ... f_1 ... ...)))))])
+(define-metafunction javalite field-lookup : object f -> loc [(field-lookup object f_target)
+(restricted-field-lookup (restrict-object object) f_target)])
+(C_c (C_0 [f_0 loc_0]
+(C_c [f_c loc_c] ...))])
+(define-metafunction javalite
+class-name : CL -> C
+[(class-name (class C_t extends C ([T f] ...) (M ...)))
+C_t ])
+(define-metafunction javalite
+parent-name : CL -> C
+[(parent-name (class C extends C_p ([T f] ...) (M ...)))
+C_p ])
+62
+
+(define-metafunction javalite
+field-list : CL -> ([T f] ...)
+[(field-list (class C extends C_p ([T f] ...) (M ...)))
+([T f] ...)])
+(define-metafunction javalite class-list-extend : (C ...) C -> (C ...) [(class-list-extend (C_0 ...) C_1)
+(C_0 ... C_1)])
+(define-metafunction javalite
+class-lookup : C -> CL
+[(class-lookup (CL_0 ... CL_1 CL_2 ...) C)
+CL_1
+(side-condition (equal? (term (class-name CL_1)) (term C)))])
+(define-metafunction javalite
+class-list-from-object : object -> (C ...) [(class-list-from-object (C_0 (C_1 [f_1 loc_1] ...) ...))
+; Restrict out the current cast -- Object will be first class
+(C_1 ...)])
+(define-metafunction javalite class-parents+self : C -> (C ...) [(class-parents+self Object)
+(class-list-extend () Object)]
+; id retricts out the base case above
+[(class-parents+self id) (class-list-extend (class-parents+self (where CL (class-lookup id))
+(where C_p (parent-name CL))])
+C_p) id)
+(define-metafunction javalite
+field-lists-extend : (([T f] ...) ...) ([T f] ...) -> (([T f] ...) ...) [(field-lists-extend (([T_0 f_0] ...) ...) ([T_1 f_1] ...))
+(([T_0 f_0] ...) ... ([T_1 f_1] ...))])
+(define-metafunction javalite fields-parents+self : C -> (([T f] ...) ...) [(fields-parents+self Object)
+(field-lists-extend () ())]
+63
+
+; id restricts out the base case above
+[(fields-parents+self id) (field-lists-extend (fields-parents+self (where CL (class-lookup id))
+(where C_p (parent-name CL))
+(where ([T f] ...) (field-list CL))])
+(define-metafunction javalite method-name : M -> m
+[(method-name (T_0 m ([T_1 x] ...) e))
+m])
+(define-metafunction javalite method-expression : M -> e [(method-expression (T_0 m ([T_1 x] ...) e))
+e])
+(define-metafunction javalite method-args : M -> (x ...) [(method-args (T_0 m ([T_1 x] ...) e))
+(x ...)])
+C_p) ([T f] ...))
+(define-metafunction javalite
+method-lookup : CL m -> any
+[(method-lookup (class C_0 extends C_1 ([T x] ...) (M_0 ... M_t M_1 ...)) m)
+(C_0 (method-args M_t) (method-expression M_t))
+(side-condition (equal? (term (method-name M_t)) (term m)))] [(method-lookup (class C_0 extends C_1 ([T x] ...) (M ...)) m)
+error
+(side-condition (equal? (findf (? (i) (equal? (term (method-name ,i)) (term m)))
+(define (->bool v) (if v
+’true ’false))
+(define-metafunction javalite
+cast : object C -> object
+[(cast (C_1 (C_2 [f_2 loc_2] ...) ...
+(C_3 [f_3 loc_3] ...)
+(term (M ...))) #f))])
+64
+
+(C_4 [f_4 loc_4] ...) ...) C_3) (C_3 (C_2 [f_2 loc_2] ...) ...
+(C_3 [f_3 loc_3] ...)
+(C_4 [f_4 loc_4] ...) ...)])
+(define (cast? object C_t) (define inner-cast?
+(term-match/single
+javalite
+[(C_1 (C_2 [f_2 loc_2] ...) ...)
+(term (C_1 C_2 ...))]))
+(if (member C_t (inner-cast? object)) #t #f))
+(define (cast?/->bool object C_t) (->bool (cast? object C_t)))
+(define-metafunction javalite
+instanceof* : object C -> v
+[(instanceof* (C_1 (C_2 [f_2 loc_2] ...) ...) C_t)
+,(->bool (member (term C_t) (term (C_2 ...))))])
+(define-metafunction javalite inject : P -> state [(inject ( (C m)))
+( mt mt ((new C) @ m ()) ret)])
+(define-metafunction javalite inject/with-state : state m -> state [(inject/with-state ( h ? e k) m)
+( h?(e@m())ret)])
