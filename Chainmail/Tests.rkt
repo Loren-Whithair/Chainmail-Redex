308b9124564b3c1b94a8faf39e4f859b52c566dc
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-01-19 13:46:12 +1300

    Re-added module tests

--- Chainmail/Tests.rkt
+++ Chainmail/Tests.rkt
@@ -199,11 +199,17 @@
   (define Loo_M? (redex-match? Loo M))
 
   (define true_Modules (list
-
+                        (term mt)
+                        (term (mt [C1 -> ('class C1() {})]))
+                        (term ((mt [C1 -> ('class C1() {})]) [C2 -> ('class C2() {})]))
+                        (term (mt [C1 -> ('class C1(x1) {})]))
+                        (term (mt [C1 -> ('class C(arg1 arg2) { ('field f_1) ('field f_2) (constructor(arg1 arg2) { () }) (method m() { () }) (ghost f(x y) { x }) })]))
                       ))
 
   (define false_Modules (list
-                       
+                       (term (mt))
+                       (term ([C1 -> ('class C1() {})]))
+                       (term (([C1 -> ('class C1() {})]) [C2 -> ('class C2() {})]))
                        ))
 
   (for ([Modules true_Modules])
