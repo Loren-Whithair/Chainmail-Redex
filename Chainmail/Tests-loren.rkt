adec2ce606c5598e3cd66698b78e8406f671c10a
loren_whithair <Loren-Whithair@users.noreply.github.com>
2021-01-20 12:18:48 +1300

    Stack tests

--- Chainmail/Tests-loren.rkt
+++ Chainmail/Tests-loren.rkt
@@ -328,12 +328,19 @@
   (define Machine_stack? (redex-match? Loo-Machine ψ))
 
   (define true_stacks (list
-                        ;terms here
+                        (term (() mt))
+                        (term ((() mt) · (() mt)))
+                        (term ((() mt) · ((() mt) · (() mt))))
+                        (term (((x1 @ f1 := x2) mt) · (() mt)))
+                        (term ((() ((mt [x1 -> 10]) [x2 -> 20])) · ((x := * $ (x2 := x4 @ mtd())) ((mt [x1 -> 10]) [x2 -> 20]))))
                         ))
 
   (define false_stacks (list
-                         ;terms here
-                         ))
+                        (term (() (mt)))
+                        (term ((() (mt)) · (() mt)))
+                        (term ((() mt) · (() (mt))))
+                        (term ((((x1 @ f1 := x2) $ (x := * $ ())) mt) · (() mt)))
+                        ))
 
   (for ([stacks true_stacks])
     (test-equal (Machine_stack? stacks) #true))
