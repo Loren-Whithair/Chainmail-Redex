be55566d24eefe09d624459b252f9d3814c3d162
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-01-29 15:48:10 +1300

    Testing reductions that don't fulfil side-conditions

--- Chainmail/Tests/reductionTests/varAssgn_OS.rkt
+++ Chainmail/Tests/reductionTests/varAssgn_OS.rkt
@@ -9,26 +9,40 @@
   (display "-------------------------------------")
   (display "\nRunning varAssgn_OS Tests:\n")
 
-  (test-->>
+  ;----------------
+  ;---TRUE TESTS---
+  ;----------------
+
+  
+  (test-->
    expr-reductions
    (term ((mt [C1 -> (clss C1() {})]) (((((x_0 := x_1 @ f1) $ ()  ) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 (mt [f1 -> true]))]) [2 -> (C1 (mt [f1 -> false]))]))))
-   (term ((mt (C1 -> (clss C1 () ())))(((()
-    (((mt (x_1 -> 2))
-      (this -> 1))
-     (x_0 -> false)))
-   ·
-   (() mt))
-  ((mt
-    (1
-     ->
-     (C1 (mt (f1 -> true)))))
-   (2
-    ->
-    (C1
-     (mt (f1 -> false)))))))))
+   (term ((mt (C1 -> (clss C1 () ())))(((() (((mt (x_1 -> 2))(this -> 1))(x_0 -> false))) · (() mt)) ((mt (1 -> (C1 (mt (f1 -> true))))) (2 -> (C1 (mt (f1 -> false)))))))))
 
-  ;;PUT TESTS HERE
 
+  ;----------------
+  ;---FALSE TESTS--
+  ;----------------
+
+
+  ;(test-->  ;;looking for a field that isn't there, can't reduce
+  ; expr-reductions
+  ; (term ((mt [C1 -> (clss C1() {})]) (((((x_0 := x_1 @ f_no) $ ()  ) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 (mt [f1 -> true]))]) [2 -> (C1 (mt [f1 -> false]))]))))
+  ; (term ((mt [C1 -> (clss C1() {})]) (((((x_0 := x_1 @ f_no) $ ()  ) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 (mt [f1 -> true]))]) [2 -> (C1 (mt [f1 -> false]))])))))
+  ;;NOTE: runs into an error because the field-lookup metafunction relies on the fact that the field you are searching for EXISTS IN THE OBJECT
+
+  
+  (test-->  ;;Class(this) ≠ Class(x_1)
+   expr-reductions
+   (term ((mt [C1 -> (clss C1() {})]) (((((x_0 := x_1 @ f1) $ ()  ) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 (mt [f1 -> true]))]) [2 -> (C2 (mt [f1 -> false]))]))))
+   ;nothing
+   )
+  
+  (test-->  ;;x_1 does NOT point to an address, poins to integer: [10]
+   expr-reductions
+   (term ((mt [C1 -> (clss C1() {})]) (((((x_0 := x_1 @ f1) $ ()  ) ((mt [x_1 -> [10]]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 (mt [f1 -> true]))]) [2 -> (C1 (mt [f1 -> false]))]))))
+   ;nothing
+   )
   
   (test-results)
   (display "-------------------------------------")
