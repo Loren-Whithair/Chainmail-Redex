b5f5fd58817cb400f319230619b9eaf4f60ea2c7
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-01-29 14:18:54 +1300

    Create get-classname.rkt to test the metafunction

---
+++ Chainmail/Tests/metafunctionTests/get-classname.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(require redex)
+(require "../../Loo.rkt")
+
+(provide test-get-classname)
+
+(define (test-get-classname)
+
+  
+  (display "-------------------------------------")
+  (display "\nRunning varAssgn_OS Tests:\n")
+
+
+  ;;PUT TESTS HERE
+  
+  (test-results)
+  (display "-------------------------------------")
+
+  )
+
