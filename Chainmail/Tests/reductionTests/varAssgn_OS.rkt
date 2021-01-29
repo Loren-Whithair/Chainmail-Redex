f828947cfb50e6615c74091289183b8ba1555b08
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-01-29 15:00:21 +1300

    One true test case

--- Chainmail/Tests/reductionTests/varAssgn_OS.rkt
+++ Chainmail/Tests/reductionTests/varAssgn_OS.rkt
@@ -9,8 +9,26 @@
   (display "-------------------------------------")
   (display "\nRunning varAssgn_OS Tests:\n")
 
+  (test-->>
+   expr-reductions
+   (term ((mt [C1 -> (clss C1() {})]) (((((x_0 := x_1 @ f1) $ ()  ) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 (mt [f1 -> true]))]) [2 -> (C1 (mt [f1 -> false]))]))))
+   (term ((mt (C1 -> (clss C1 () ())))(((()
+    (((mt (x_1 -> 2))
+      (this -> 1))
+     (x_0 -> false)))
+   ·
+   (() mt))
+  ((mt
+    (1
+     ->
+     (C1 (mt (f1 -> true)))))
+   (2
+    ->
+    (C1
+     (mt (f1 -> false)))))))))
 
   ;;PUT TESTS HERE
+
   
   (test-results)
   (display "-------------------------------------")
