605c7981473dee78c4d32511df9f0bda5cf74128
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-01-29 14:05:46 +1300

    Added a functioning varAssgn reduction in comments

--- Chainmail/Tests/reductionTests/varAssgn_OS.rkt
+++ Chainmail/Tests/reductionTests/varAssgn_OS.rkt
@@ -24,7 +24,9 @@ Attempting to see if the reduction works:
 
 (traces
    expr-reductions
-   (term ((mt [C1 -> (clss C1() {})]) (((((x_0 := x_1 @ f) $ ()  ) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 (mt [f1 -> true]))]) [2 -> (C1 (mt [f1 -> false]))])))))
+   (term ((mt [C1 -> (clss C1() {})]) (((((x_0 := x_1 @ f1) $ ()  ) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 (mt [f1 -> true]))]) [2 -> (C1 (mt [f1 -> false]))])))))
+
+--THIS WORKS
 
 |#
 
