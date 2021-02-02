04ed47d0bd26056396027cd671771ac4736ab6e6
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-02-02 15:51:53 +1300

    Added false tests

--- Chainmail/Tests/reductionTests/methCall_OS.rkt
+++ Chainmail/Tests/reductionTests/methCall_OS.rkt
@@ -9,6 +9,11 @@
   (display "-------------------------------------")
   (display "\nRunning methCall_OS Tests:\n")
 
+  ;----------------
+  ;---TRUE TESTS---
+  ;----------------
+
+  
   (test-->
     expr-reductions
    (term ((mt [C1 -> (clss C1() { (method m_0() { (return something) }) })]) (((((x_0 := x_1 @ m_0()) $ ()) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ((mt [1 -> (C1 mt)]) [2 -> (C1 mt)]))))
@@ -23,6 +28,35 @@
    expr-reductions
    (term ((mt [C1 -> (clss C1() { (method m_0(arg1 arg2) { (return something) }) })]) (((((x_0 := x_1 @ m_0(param1 param2)) $ ()) ((((mt [x_1 -> 2]) [this -> 1]) [param1 -> null]) [param2 -> null])) · (() mt)) ((mt [1 -> (C1 mt)]) [2 -> (C1 mt)]))))
    (term ((mt (C1 -> (clss C1() ((method m_0(arg1 arg2) ((return something))))))) ((((return something) (((mt (arg1 -> null)) (arg2 -> null)) (this -> 2))) · (((x_0 := * $ ()) ((((mt (x_1 -> 2)) (this -> 1)) (param1 -> null)) (param2 -> null))) · (() mt))) ((mt (1 -> (C1 mt))) (2 -> (C1 mt)))))))
+
+
+  ;----------------
+  ;---FALSE TESTS--
+  ;----------------
+
+  (test-->
+   expr-reductions
+   (term (((mt [C1 -> (clss C1() {})]) [C2 -> (clss C2() {})]) ((() mt) mt)))
+   ;;nothing
+   )
+
+  (test-->  ;; x_1 not an addr (i.e. not an object)
+   expr-reductions
+   (term ((mt [C1 -> (clss C1() { (method m_0() { (return something) }) })]) ;;Module
+          (((((x_0 := x_1 @ m_0()) $ ()) ((mt [x_1 -> null]) [this -> 1])) · (() mt)) ;;Stack
+           ((mt [1 -> (C1 mt)]) [2 -> (C1 mt)])))) ;;Heap
+   ;;nothing
+   )
+   
+  
+  (test-->  ;; (M-Match M C_1) ≠ #t    - the class of x_1 is not defined in M
+   expr-reductions
+   (term ((mt [C2 -> (clss C2() { (method m_0() { (return something) }) })]) ;;Module
+          (((((x_0 := x_1 @ m_0()) $ ()) ((mt [x_1 -> 2]) [this -> 1])) · (() mt)) ;;Stack
+           ((mt [1 -> (C1 mt)]) [2 -> (C1 mt)])))) ;; Heap
+   ;;nothing
+   )
+
   
   (test-results)
   (display "-------------------------------------")
