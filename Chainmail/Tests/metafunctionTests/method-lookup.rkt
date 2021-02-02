fb73225eb090c32655206ad956c26c0a15717d3c
David Franz <davidfranznz@gmail.com>
2021-02-02 14:49:21 +1300

    added method-lookup tests

--- Chainmail/Tests/metafunctionTests/method-lookup.rkt
+++ Chainmail/Tests/metafunctionTests/method-lookup.rkt
@@ -15,8 +15,15 @@
   (display "\nRunning method-params Tests:\n")
 
   (test-equal (term (mf-apply method-lookup (clss C() { (method m_0() { () }) }) m_0)) (term (method m_0() { () })))
-  (test-equal (term (mf-apply method-lookup (clss C() { (method m_0(x) { () }) }) m_0)) (term (method m_0(x) { () })))
-  (test-equal (term (mf-apply method-lookup (clss C() { (method m_0(x y) { () }) }) m_0)) (term (method m_0(x y) { () })))
+  (test-equal (term (mf-apply method-lookup (clss C() { (method m_0(x_0) { () }) }) m_0)) (term (method m_0(x_0) { () })))
+  (test-equal (term (mf-apply method-lookup (clss C() { (method m_0(y_0 x_0) { () }) }) m_0)) (term (method m_0(y_0 x_0) { () })))
+
+  (test-equal (term (mf-apply method-lookup (clss C() { (method m_0() { ((method_result := x @ m(arg1 arg2)) $ (object_result := new C(arg1 arg2))) }) }) m_0))
+              (term (method m_0() { ((method_result := x @ m(arg1 arg2)) $ (object_result := new C(arg1 arg2))) })))
+
+  (test-equal (term (mf-apply method-lookup (clss C() { (method m_0(x_0) { ((x @ f := y) $ (return x)) }) }) m_0)) (term (method m_0(x_0) { ((x @ f := y) $ (return x)) })))
+
+  (test-equal (term (mf-apply method-lookup (clss C() { (method m_0(y_0 x_0) { (() $ (() $ ())) }) }) m_0)) (term (method m_0(y_0 x_0) { (() $ (() $ ())) })))
   
   (test-results)
   (display "-------------------------------------")
