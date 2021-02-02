672a8ef9d56903f8c17a1cd5e942b79cbcc4a248
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-02-02 14:24:49 +1300

    Added tests

--- Chainmail/Tests/metafunctionTests/method-Stmts.rkt
+++ Chainmail/Tests/metafunctionTests/method-Stmts.rkt
@@ -8,8 +8,14 @@
 
   (display "-------------------------------------")
   (display "\nRunning method-Stmts Tests:\n")
-               
+
+  (test-equal (term (mf-apply method-Stmts (method m() {()}))) (term ()))  ;;empty stmt list
+  
+  (test-equal (term (mf-apply method-Stmts (method m(arg1 arg2) { (z := y @ f) }))) (term (z := y @ f)))  ;;Stmts == Stmt
+  
+  (test-equal (term (mf-apply method-Stmts (method m(arg1) { ((x2 @ f1 := x1) $ (x1 := x2 @ m1(arg2))) })))
+              (term ((x2 @ f1 := x1) $ (x1 := x2 @ m1(arg2))))) ;;Stmts == (Stmt $ Stmt)
+  
   (test-results)
   (display "-------------------------------------")
   )
-
--- Chainmail/Tests/metafunctionTests/method-params.rkt
+++ Chainmail/Tests/metafunctionTests/method-params.rkt
@@ -8,8 +8,12 @@
 
   (display "-------------------------------------")
   (display "\nRunning method-params Tests:\n")
-               
+
+  (test-equal (term (mf-apply method-params (method m() {()}))) (term ()))   ;;empty method
+  (test-equal (term (mf-apply method-params (method m(arg1) {()}))) (term (arg1)))  ;;one arg
+  (test-equal (term (mf-apply method-params (method m(arg1 arg2) { (z := y @ f) }))) (term (arg1 arg2))) ;;multiple args
+  (test-equal (term (mf-apply method-params (method myMeth(x1 x2 x3 otherparam) {()}))) (term (x1 x2 x3 otherparam)))
+                              
   (test-results)
   (display "-------------------------------------")
   )
-
