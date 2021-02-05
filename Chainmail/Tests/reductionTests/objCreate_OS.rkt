319f3f866b85eb0e16b58c165b2f0def35b6890a
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-02-05 14:59:16 +1300

    One true test for reduction

--- Chainmail/Tests/reductionTests/objCreate_OS.rkt
+++ Chainmail/Tests/reductionTests/objCreate_OS.rkt
@@ -14,6 +14,22 @@
   ;---TRUE TESTS---
   ;----------------
 
+  (test-->
+   expr-reductions
+   (term
+    ((mt [myClass -> (clss myClass(arg1 arg2) { (constructor(a1 a2) { (return this) } )})])  ;;Module
+     (((((x_0 := new myClass(p1 p2)) $ ()) (((mt [p1 -> 1]) [p2 -> 2]) [this -> 3]))        ;;Next frame
+       · (() mt))  ;;Stack
+      (((mt [1 -> (C1 mt)]) [2 -> (C2 mt)]) [3 -> (C3 mt)])))   ;;Heap
+    )
+
+  (term
+   ((mt (myClass -> (clss myClass (arg1 arg2) ((constructor (a1 a2) ((return this)))))))  ;;Module
+    ((((return this) (((mt (a1 -> 1))(a2 -> 2)) (this -> 4)))  ;New frame 
+   · (((x_0 := * $ ()) (((mt (p1 -> 1)) (p2 -> 2)) (this -> 3)))  ;;Continuation frame
+    · (() mt)))  ;;Stack
+     ((((mt (1 -> (C1 mt))) (2 -> (C2 mt))) (3 -> (C3 mt))) (4 -> (myClass mt)))))))  ;;Heap
+  
   ;----------------
   ;---FALSE TESTS--
   ;----------------
