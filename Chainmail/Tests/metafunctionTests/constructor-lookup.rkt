390f50f4df72706d76845ca177e1eb14c2dd4dde
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-02-05 15:14:02 +1300

    Added tests

--- Chainmail/Tests/metafunctionTests/constructor-lookup.rkt
+++ Chainmail/Tests/metafunctionTests/constructor-lookup.rkt
@@ -10,12 +10,20 @@
   (display "-------------------------------------")
   (display "\nRunning cosntructor-lookup Tests:\n")
 
-  (test-equal (term (mf-apply constructor-lookup (clss C() {}))) (term (constructor() { () })))
-  (test-equal (term (mf-apply constructor-lookup (clss C() {(constructor(x_1 x_2) { (x1 := y1 @ f) })}))) (term (constructor(x_1 x_2) { (x1 := y1 @ f) })))
+  (test-equal (term (mf-apply constructor-lookup (clss C() {}))) (term (constructor() { (return this) })))
+  (test-equal (term (mf-apply constructor-lookup (clss C(arg1 arg2) { (fld f_1) (fld f_2) (method m(arg1 arg2) { () }) (ghost f(x y) { x }) })))
+              (term (constructor() { (return this) })))
   
+  (test-equal (term (mf-apply constructor-lookup (clss C() {(constructor(x_1 x_2) { (x1 := y1 @ f) })}))) (term (constructor(x_1 x_2) { (x1 := y1 @ f) })))
+
+  (test-equal (term (mf-apply constructor-lookup (clss C() { (fld f) (constructor() { (x1 @ f1 := y1) }) (method m() { () }) }))) (term (constructor() { (x1 @ f1 := y1) })))
+  (test-equal (term (mf-apply constructor-lookup (clss C() { (fld f) (constructor() { (x1 := y1 @ f1) }) (method m() { () }) }))) (term (constructor() { (x1 := y1 @ f1) })))
+
+  (test-equal (term (mf-apply constructor-lookup  (clss C(arg1 arg2) { (fld f_1) (fld f_2) (constructor(arg1 arg2) { () }) (method m() { () }) (ghost f(x y) { x }) })))
+              (term (constructor(arg1 arg2) { () })))
+
+                      
   (test-results)
 
   (display "-------------------------------------")
   )
-
-(test-constructor-lookup)
