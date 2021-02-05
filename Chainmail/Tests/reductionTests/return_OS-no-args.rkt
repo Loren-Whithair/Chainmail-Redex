a2cd33621cc7857f52874df85739b8c2dc5b6634
David Franz <davidfranznz@gmail.com>
2021-02-05 15:35:55 +1300

    reformatted tests

--- Chainmail/Tests/reductionTests/return_OS-no-args.rkt
+++ Chainmail/Tests/reductionTests/return_OS-no-args.rkt
@@ -14,6 +14,32 @@
   ;---TRUE TESTS---
   ;----------------
 
+  (test-->
+   expr-reductions
+   (term ((mt (C1 -> (clss C1() ((method m_0() ((return this))))))) ;; module
+          ((((return this) (mt (this -> 2))) ;; top frame
+            · (((x_0 := * $ ()) ((mt (x_1 -> 2)) (this -> 1))) ;; next frame
+               · (() mt))) ;; bottom frame
+           ((mt (1 -> (C1 mt))) (2 -> (C1 mt)))))) ;; heap
+
+   (term ((mt (C1 -> (clss C1() ((method m_0() ((return this))))))) ;; module
+          (((() (((mt (x_1 -> 2)) (this -> 1)) (x_0 -> 2))) ;; top frame
+            · (() mt)) ;; bottom frame
+           ((mt (1 -> (C1 mt))) (2 -> (C1 mt))))))) ;; heap
+
+  (test-->
+   expr-reductions
+   (term ((mt (C1 -> (clss C1() ( (method m_0() ((return x_0))))))) ;; module
+          ((((return x_0) ((mt (this -> 2)) (x_0 -> true))) ;; top frame
+            · (((x_0 := * $ ()) ((mt (x_1 -> 2)) (this -> 1))) ;; next frame
+               · (() mt))) ;; bottom frame
+           ((mt (1 -> (C1 mt))) (2 -> (C1 mt)))))) ;; heap
+
+   (term ((mt (C1 -> (clss C1() ((method m_0() ((return x_0))))))) ;; module
+          (((() (((mt (x_1 -> 2)) (this -> 1)) (x_0 -> true))) ;; top frame
+            · (() mt)) ;; bottom frame
+           ((mt (1 -> (C1 mt))) (2 -> (C1 mt))))))) ;; heap
+
   ;----------------
   ;---FALSE TESTS--
   ;----------------
