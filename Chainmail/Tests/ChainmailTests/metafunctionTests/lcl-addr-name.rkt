0f014dbccf8e3258a40f61c69484ecf6ff98dae6
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-02-09 16:40:08 +1300

    Altered file structuring

---
+++ Chainmail/Tests/ChainmailTests/metafunctionTests/lcl-addr-name.rkt
@@ -0,0 +1,16 @@
+#lang racket
+(require redex)
+(require "../../Loo.rkt")
+
+(provide test-lcl-addr-name)
+
+(define (test-lcl-addr-name)
+
+  (display "-------------------------------------")
+  (display "\nRunning lcl-addr-name Tests:\n")
+
+                        
+  (test-results)
+  (display "-------------------------------------")
+  )
+
