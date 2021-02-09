23578644d66fc86bf55c0bf6c44ce9a176162200
David Franz <davidfranznz@gmail.com>
2021-02-09 15:28:46 +1300

    added Chainmail metafunction tests

--- Chainmail/Tests/Chainmail-metafunctionTests/addr-in-fieldMap.rkt
+++ Chainmail/Tests/Chainmail-metafunctionTests/addr-in-fieldMap.rkt
@@ -10,7 +10,12 @@
   (display "-------------------------------------")
   (display "\nRunning addr-in-fieldMap Tests:\n")
 
-  (test-equal (term (mf-apply addr-in-fieldMap (mt [f_0 -> 0]) 0)) #false)
+  (test-equal (term (mf-apply addr-in-fieldMap mt 0)) #false)
+  (test-equal (term (mf-apply addr-in-fieldMap (mt [f_0 -> 0]) 1)) #false)
+  (test-equal (term (mf-apply addr-in-fieldMap ((mt [f_0 -> 0]) [f_1 -> 1]) 2)) #false)
+
+  (test-equal (term (mf-apply addr-in-fieldMap (mt [f_0 -> 0]) 0)) #true)
+  (test-equal (term (mf-apply addr-in-fieldMap ((mt [f_0 -> 0]) [f_1 -> 1]) 1)) #true)
   
   (test-results)
   (display "-------------------------------------")
--- Chainmail/Tests/Chainmail-metafunctionTests/addr-in-lcl.rkt
+++ Chainmail/Tests/Chainmail-metafunctionTests/addr-in-lcl.rkt
@@ -10,6 +10,13 @@
   (display "-------------------------------------")
   (display "\nRunning addr-in-lcl Tests:\n")
 
+
+  (test-equal (term (mf-apply addr-in-lcl mt 0)) #false)
+  (test-equal (term (mf-apply addr-in-lcl (mt [x_0 -> 0]) 1)) #false)
+  (test-equal (term (mf-apply addr-in-lcl ((mt [x_0 -> 0]) [f_1 -> 1]) 2)) #false)
+
+  (test-equal (term (mf-apply addr-in-lcl (mt [x_0 -> 0]) 0)) #true)
+  (test-equal (term (mf-apply addr-in-lcl ((mt [x_0 -> 0]) [x_1 -> 1]) 1)) #true)
   
   (test-results)
   (display "-------------------------------------")
