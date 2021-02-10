908c3769247ccb710e32811e3c4b77706e43f0a9
Loren Whithair <whithalore@saigon-van.ecs.vuw.ac.nz>
2021-02-10 13:39:10 +1300

    Annotated helper functions

--- Chainmail/Loo.rkt
+++ Chainmail/Loo.rkt
@@ -196,10 +196,8 @@ To consider: what does the fieldAssgn and varAssgn reductions do when the variab
         (where ClassDesc_0 (CD-lookup M C_0))
 
         ;; Obtain the method body and list of required arguments
-        (where MethDecl_0 (method-lookup ClassDesc_0 m))
-        (where Stmts_0 (method-Stmts MethDecl_0))
-        (where (x_3 ...) (method-params MethDecl_0))
-
+        (where (method m(x_3 ...) { Stmts_0 }) (method-lookup ClassDesc_0 m))
+        
         ;; Create the new frame where the Stmts are those of the method body, and η is as defined below:
         (where η_1 (η-extend* (mt [this -> addr_0]) [x_3 -> (η-lookup η x_2)] ...))  ;; [this -> (the object the method was invoked on)], followed by [parameters -> arguments given]
         (where Φ_1 (Stmts_0 η_1))
@@ -309,67 +307,62 @@ To consider: what does the fieldAssgn and varAssgn reductions do when the variab
   [(h-max (χ [addr -> Object]))
    ,(max (term addr) (term (h-max χ)))])
 
-
+;; new-addr : generates a number that is NOT yet used as an address in the heap χ
 (define-metafunction Loo-Machine
   new-addr : χ -> addr
   [(new-addr χ)
    ,(add1 (term (h-max χ)))])
 
 
-;------------------------------
-;-------Simple functions-------
-
+;; get-classname : obtains the ClassID of a given Object
 (define-metafunction Loo-Machine
   get-classname : Object -> C
   [(get-classname (C fieldMap)) C])
-
-(define-metafunction Loo-Machine
-  method-Stmts : MethDecl -> Stmts
-  [(method-Stmts (method m_0(x ...) { Stmts })) Stmts])
-
-(define-metafunction Loo-Machine
-  method-params : MethDecl -> (x ...)
-  [(method-params (method m_0(x ...) { Stmts })) (x ...)])
-
 
 
 ;------------------------------
 ;----Search through mappings---
 
+;; h-lookup : find the Object pointed to by an addr in χ
 (define-metafunction Loo-Machine
   h-lookup : χ addr -> Object
   [(h-lookup χ addr)
    (storelike-lookup χ addr)])
 
+;; η-lookup : find the value pointed to by an varID in η
 (define-metafunction Loo-Machine
   η-lookup : η x -> v
   [(η-lookup η x)
    (storelike-lookup η x)])
 
+;; field-lookup : find the value pointed to by a field in an Object's fieldMap
 (define-metafunction Loo-Machine
   field-lookup : Object f -> v
   [(field-lookup (C fieldMap) f)
    (storelike-lookup fieldMap f)])
 
+;; CD-lookup : find the ClassDescription of a given ClassID in a Module 'M'
 (define-metafunction Loo-Machine
   CD-lookup : M C -> ClassDesc
   [(CD-lookup M C)
    (storelike-lookup M C)])
 
+;; method-lookup : find the MethDecl of a method with the ID 'm', in a ClassDesc
 (define-metafunction Loo-Machine
   method-lookup : ClassDesc m -> MethDecl
   [(method-lookup (clss C(x ...) { FieldDecl ... CDecl ... MethDecl ... (method m_0(x_0 ...) { Stmts }) MethDecl ...  GhostDecl ... }) m_0)
    (method m_0(x_0 ...) { Stmts })])
 
+
+;; constructor-lookup : find the constructor declaration in a ClassDesc, or give a default if not found
 (define-metafunction Loo-Machine
   constructor-lookup : ClassDesc -> CDecl
   [(constructor-lookup (clss C(x_0 ...) { FieldDecl ... (constructor(x_1 ...) { Stmts_0 }) MethDecl ... GhostDecl ... }))
    (constructor(x_1 ...) { Stmts_0 })]
   [(constructor-lookup (clss C(x ...) { FieldDecl ... MethDecl ... GhostDecl ... }))
-   (constructor() { (return this) })])  ;;maybe remove return this later
+   (constructor() { (return this) })]) 
 
-
-
+;; M-match : confirm whether or not a ClassID has a ClassDesc in the module 'M'
 (define-metafunction Loo-Machine
   M-match : M C -> boolean
   [(M-match mt any_0) #false]
@@ -379,65 +372,91 @@ To consider: what does the fieldAssgn and varAssgn reductions do when the variab
    (side-condition (not (equal? (term C_1) (term C_2))))])
 
 
-
 ;------------------------------
-;------Adding to mappings------
+;------Extending mappings------
 
+;; h-extend* : add 0 or more new mappings to χ
 (define-metafunction Loo-Machine
   h-extend* : χ [addr -> Object] ... -> χ ;; takes in a arbitrary number of mappings
   [(h-extend* χ [addr -> Object] ...)
    ,(storelike-extend* <= (term χ) (term ([addr -> Object] ...)))])
 
+;; η-extend* : add 0 or more new mappings to η
 (define-metafunction Loo-Machine
   η-extend* : η [x -> v] ... -> η
   [(η-extend* η [x -> v] ...)
    ,(storelike-extend* id-<= (term η) (term ([x -> v] ...)))])
 
+;; Object-extend* : add 0 or more new mappings to the fieldMap inside an Object
 (define-metafunction Loo-Machine
   Object-extend* : Object [f -> v] ... -> Object
   [(Object-extend* (C_0 fieldMap) [f -> v] ...)
    (C_0 (mf-apply fieldMap-extend* fieldMap [f -> v] ...))]) 
 
+;; fieldMap-extend* : add 0 or more new mappings to fieldMap
 (define-metafunction Loo-Machine
   fieldMap-extend* : fieldMap [f -> v] ... -> fieldMap
   [(fieldMap-extend* fieldMap [f -> v] ...)
    ,(storelike-extend* id-<= (term fieldMap) (term ([f -> v] ...)))])
 
 
-
 ;------------------------------
 ;------Auxiliary functions-----
- 
+
+
+;; NOTE: all these functions have been taken from https://github.com/ericmercer/javalite
+
+
+;; storelike-lookup : takes a recursive mapping structure mt | (list [key -> val]) and finds the value associated with the given key
 (define-metafunction Loo-Machine
   storelike-lookup : any any -> any
-  ; [(storelike-lookup mt any_0) #false] ;; unable to find anything in an empty 'any' (for example, an object)
-  [(storelike-lookup (any_0 [any_t -> any_ans]) any_t)
-   any_ans] ;; if any_t points to any_ans in any_0, we return any_ans
-  [(storelike-lookup (any_0 [any_k -> any_v]) any_t)
-   (storelike-lookup any_0 any_t)
-   (side-condition (not (equal? (term any_k) (term any_t))))]) ;; ensures any_k != any_t (otherwise we would match the previous condition)
 
+  [(storelike-lookup (any_0 [any_t -> any_ans]) any_t) ;; if the last key in the mapping is what we are searching for: 'any_t' ... 
+   any_ans] ; ---------------------------------------- ;; ...we return what it poins to: 'any_ans'
 
+  [(storelike-lookup (any_0 [any_k -> any_v]) any_t) ; ------- ;; if the last value in the mapping is not any_t...
+   (storelike-lookup any_0 any_t) ; -------------------------- ;; ...then recursively call on the rest of the mapping...
+   (side-condition (not (equal? (term any_k) (term any_t))))]) ;; ...so long as any_k != any_t (otherwise we would match the previous condition)
 
+;; id-<=
 (define (id-<= a b)
   (string<=? (symbol->string a) (symbol->string b)))
+
+
+
+;; storelike-extend : takes one mapping and extends the recursive map structure to include this mapping
+;; Maintains ordering of keys
 
 ;; if 'storelike' is empty, return just the new mapping
 ; else insert the new mapping [k -> hv] while keeping the ordering of the keys
 (define (storelike-extend <= storelike k hv)  ;;storelike: the map we're extending, k: the key, hv: the value
   (match storelike
-    ['mt `(mt [,k -> ,hv])]
-    [`(,storelike [,ki -> ,hvi])
+    ['mt `(mt [,k -> ,hv])]  ;; if it's an empty mapping, return a new map containing just [k -> hv]
+        
+    [`(,storelike [,ki -> ,hvi])  ;;else ...
      (cond
-       [(equal? k ki)   ;; if the key is already in the mapping, just replace the value it maps to 
-        `(,storelike [,ki -> ,hv])]
-       [(<= k ki)  ;;otherwise, if k <= ki, recursively call one item back in the list to check if that one is equal to k
-        `(,(storelike-extend <= storelike k hv) [,ki -> ,hvi])]
-       [else
-        `((,storelike [,ki -> ,hvi]) [,k -> ,hv])])]))     ;;if you either get to the end or find the spot where [k -> hv] fits in the ordering, put it there
 
+       [(equal? k ki)  ; ----------- ;; if the last element in the mapping contains the key we are looking for...
+        `(,storelike [,ki -> ,hv])]  ;; ...then set the value of this key to the given value, 'hv'
+
+       [(<= k ki)  ; ------------------------------------------ ;; else if the key we gave has a SMALLER value than the last value in the mapping...
+        `(,(storelike-extend <= storelike k hv) [,ki -> ,hvi])] ;; ...then recursively call storelike-extend on everything before the last value
+                                                                ;; and add the last mapping [ki -> hv] to the end of the returned map
+
+       [else                                           
+        `((,storelike [,ki -> ,hvi]) [,k -> ,hv])])])) ;; else add [k -> v] onto the end of the mapping
+       ;; (note that if you have reached the above condition, then !(k <= ki) - i.e. k > ki, but k is smaller than any elements that came after ki in the original map
+       ;; and therefore, we can insert [k -> v] here while maintaining the ordering of the keys
+
+
+;; storelike-extend* : calls storelike-extend once for each element given
 (define (storelike-extend* <= storelike extend*)
   (match extend*
     ['() storelike]
     [`([,k -> ,hv] . ,extend*)
      (storelike-extend* <= (storelike-extend <= storelike k hv) extend*)]))
+
+
+; -----------------------------------------------------
+; -----------------------------------------------------
+; -----------------------------------------------------
